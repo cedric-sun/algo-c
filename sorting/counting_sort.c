1abc13f987619f91be431e1514002e07a9d4fe9c
Cedric Sun <sunce@ufl.edu>
2019-12-28 02:30:55 -0500

    add counting sort

---
+++ sorting/counting_sort.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+/* counting sort
+ *
+ * all elements in arr has to be non-negative
+ */
+unsigned int *counting_sort( unsigned int *arr, size_t size ) {
+    unsigned int max = 0;
+    for ( size_t i = 0; i < size; i++ )
+        if ( max < arr[i] ) max = arr[i];
+    unsigned int *count = calloc( max + 1, sizeof *count );
+    unsigned int *ans = malloc( size * sizeof *ans );
+    for ( size_t i = 0; i < size; i++ ) count[arr[i]]++;
+    for ( size_t i = 1; i < max + 1; i++ ) count[i] += count[i - 1];
+    for ( ptrdiff_t i = size - 1; i >= 0; i-- ) {
+        count[arr[i]]--;
+        ans[count[arr[i]]] = arr[i];
+    }
+    free( count );
+    return ans;
+}
+
+#include <stdio.h>
+#include <time.h>
+int main() {
+#define N 10000
+#define MOD 101
+    srand( time( NULL ) );
+    unsigned int *arr = malloc( N * sizeof *arr );
+    for ( size_t i = 0; i < N; i++ ) arr[i] = rand() % MOD;
+    unsigned int *ans = counting_sort( arr, N );
+    for ( size_t i = 1; i < N; i++ ) {
+        if ( ans[i] < ans[i - 1] ) {
+            fputs( "panic", stderr );
+            return -1;
+        }
+    }
+    puts( "pass" );
+    free( ans );
+    return 0;
+}
